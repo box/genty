e79a4cef5bb0f130d5dc632bf573ee3331b19123
Jeffrey Meadows <jmeadows@box.com>
2014-04-29 13:26:03 -0700

    Change genty to not use namespace packages.

--- genty/__init__.py
+++ genty/__init__.py
@@ -2,5 +2,6 @@
 
 from __future__ import unicode_literals
 from .genty import genty
+from .genty_args import genty_args
 from .genty_dataset import genty_dataset
 from .genty_repeat import genty_repeat
